2ee1e44bbb39448f9729b1caa8dfa9b4c0eca345
tran109 <tran109@sheridancollege.ca>
2020-01-16 11:26:39 -0500

    third comment

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -51,8 +51,6 @@ public class StudentList {
         for(int i = 0; i < 3; i++) {
             System.out.println("Enter Student Name");    
             String name = input.nextLine();
-               System.out.println("Enter Student Address");    
-            String name = input.nextLine();
             Student student = new Student(name);
             students[i] = student;
         }
